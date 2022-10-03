f2b7204ae30fd8a9cb50137f8622d342f83fdbf3
abdurreman_mushtaq <abdurrehman.mushtaq@gmail.com>
2022-10-03 21:42:15 +0500

    implemented your suggestions

--- books_scrape/spiders/books_data.py
+++ books_scrape/spiders/books_data.py
@@ -8,7 +8,6 @@ class BooksSpider(scrapy.Spider):
     start_urls = ['https://books.toscrape.com']
 
     def parse(self, response, **kwargs):
-
         list_of_books = response.css('ol.row li a::attr(href)').getall()
         yield from response.follow_all(list_of_books, callback=self.items)
 
@@ -16,39 +15,38 @@ class BooksSpider(scrapy.Spider):
         if next_page_url:
             yield response.follow(next_page_url, callback=self.parse)
 
-    def items(self, response):
+    def categories(self, response):
+        category = response.css('ul.breadcrumb li a::text').getall()
+        category = category[1:]
+        category = ' > '.join(category)
+        return category
 
-        sub_categories = response.css('ul.breadcrumb li a::text').getall()
-        slicing = sub_categories[1:]
-        sub_categories = ' > '.join(slicing)
-
+    def number_of_available_products(self, response):
         product_info = response.xpath('//i[@class="icon-ok"]/following-sibling::text()').get()
-        product_info = re.findall(r'\d{2}', product_info)
-        product_info = ''.join(product_info)
+        product_info = re.findall(r'\d+', product_info)
+        return product_info[0]
 
+    def stock_availability (self, response):
         availability_stock = response.xpath('//i[@class="icon-ok"]/following-sibling::text()').get()
         availability_stock = re.findall(r'\b[A-z]{2}\s[a-z]{5}\b', availability_stock)
-
-        if availability_stock:
-
-            yield {
-                'category': sub_categories,
-                'name': response.css('div.product_main h1::text').get().strip('#').strip('"'),
-                'price': response.css('p.price_color::text').get().replace('£', ''),
-                'number_of_available_products': product_info,
-                'in_stock': bool(availability_stock),
-                'description': response.css('#product_description + p::text').get(),
-                'production_information': self.fetch_product_info(response),
-                'url': response.url
-            }
+        return availability_stock
 
     def fetch_product_info(self, response):
-
         product_info = {}
-
         for info in response.css(".table-striped tr"):
             product_info_heading = info.css("th::text").get()
             product_info_value = info.css("td::text").get()
             product_info[product_info_heading] = product_info_value
-
         return product_info
+
+    def items(self, response):
+        yield {
+                'category': self.categories(response),
+                'name': response.css('div.product_main h1::text').get().strip('#').strip('"'),
+                'price': response.css('p.price_color::text').get().replace('£', ''),
+                'number_of_available_products': self.number_of_available_products(response),
+                'in_stock': bool(self.stock_availability(response)),
+                'description': response.css('#product_description + p::text').get(),
+                'production_information': self.fetch_product_info(response),
+                'url': response.url
+            }
